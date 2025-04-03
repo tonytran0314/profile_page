8186ca2127f76a9fdd0189acce486a71962ff42b
Trần Gia Huy <tonytran0314@gmail.com>
2025-04-03 18:59:44 -0400

    avatar and user full name

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -7,21 +7,77 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Bio-data",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 17,
-            fontWeight: FontWeight.w500,
-          ),
+      appBar: appBar(),
+      body: Container(
+        color: Colors.blue,
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            SizedBox(height: 40),
+            avatarAndName(),
+            SizedBox(height: 40),
+            // avatarAndName()
+          ],
         ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(16),
-          child: SvgPicture.asset('assets/icons/left-arrow.svg'),
+      ),
+    );
+  }
+
+  Container avatarAndName() {
+    return Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+                Container(
+                  width: 78,
+                  height: 78,
+                  decoration: BoxDecoration(
+                    color: Color(0xff979797),
+                    shape: BoxShape.circle
+                  ),
+                  child: Image.asset(
+                    'assets/images/avatar.png',
+                    width: 72,
+                    height: 72,
+                  ),
+                ),
+                SizedBox(height: 18),
+                Text(
+                  'Itunuoluwa Abidoye',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                Text(
+                  'Itunuoluwa@petra.africa',
+                  style: TextStyle(
+                    color: Color(0xffABABAB),
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal
+                  ),
+                ),
+              ],
+            ),
+          );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        "Bio-data",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 17,
+          fontWeight: FontWeight.w500,
         ),
+      ),
+      backgroundColor: Colors.green,
+      centerTitle: true,
+      leading: Container(
+        margin: EdgeInsets.all(16),
+        child: SvgPicture.asset('assets/icons/left-arrow.svg'),
       ),
     );
   }
