a9c1d2d43ce087f13f4d2a7443986b994e12040b
Trần Gia Huy <tonytran0314@gmail.com>
2025-04-03 01:20:56 -0400

    app bar

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
@@ -8,8 +9,16 @@ class Profile extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Profile"),
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.white,
         centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(16),
+          child: SvgPicture.asset(
+            'assets/icons/left-arrow.svg',
+            width: 16,
+            height: 16,
+          ),
+        ),
       ),
     );
   }
